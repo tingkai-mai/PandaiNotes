0f82717dfbc027828281cb88ed28715132c1427e
Nigel Teo <nigelteosw@gmail.com>
2021-12-20 13:22:39 +0800

    green sidebar exist

---
+++ react-sidebar/src/components/Sidebar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "../App.css";
+
+function Sidebar() {
+    return (
+        <div className="Sidebar">
+            
+        </div>
+    );
+}
+
+export default Sidebar;
