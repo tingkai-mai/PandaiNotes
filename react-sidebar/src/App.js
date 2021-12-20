0a7c8806712ea586487b57feb9131db48ed14d6c
Nigel Teo <nigelteosw@gmail.com>
2021-12-20 13:22:14 +0800

    sidebar is now green

--- react-sidebar/src/App.js
+++ react-sidebar/src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
+import Sidebar from './components/Sidebar';
 
 function App() {
   return (
     <div className="App">
-      <h1>Ayoo</h1>
-    </div>
-  );
+      <Sidebar />
+    </div>);
 }
 
 export default App;
