667eeabfecae0ab2ab341a191323a04ac140e3ac
Nigel Teo <nigelteosw@gmail.com>
2021-12-22 23:30:48 +0800

    integrating old code

--- PandaiNotes/src/components/UI/Navbar/Navbar.js
+++ PandaiNotes/src/components/UI/Navbar/Navbar.js
@@ -6,12 +6,25 @@ import { Nav, Navbar, NavDropdown } from "react-bootstrap";
 function NavbarInternal() {
   return (
     <Navbar className={`${classes["side-navbar"]} flex-column`}>
-      <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+      <Navbar.Brand href="#home">Pandai-Notes</Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="me-auto flex-column">
-          <Nav.Link href="#home">Home</Nav.Link>
-          <Nav.Link href="#link">Link</Nav.Link>
+          <Nav.Link className="SidebarList">
+            {NavbarData.map((val, key) => {
+              return (
+                <li
+                  key={key}
+                  className={classes.row}
+                  id={window.location.pathname === val.link ? "active" : ""}
+                  onClick={() => {window.location.pathname = val.link;}}
+                >
+                  <div id="icon">{val.icon}</div>
+                  <div id="title">{val.title}</div>
+                </li>
+              );
+            })}
+          </Nav.Link>
           <NavDropdown title="Dropdown" id="basic-nav-dropdown">
             <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
             <NavDropdown.Item href="#action/3.2">
