ac083e680e67800e39f6cee53c74392b058bb98a
Ferusel <wheatgrass.mc@gmail.com>
2021-12-22 15:27:25 +0800

    Added Bootstrap5 + Document Editor
    Changes made were:
    - Added code to let BootStrap5 work
    - Created Document Editor using TinyMCE WYSIWYG editor
    - Rewrote Nigel's Navbar code with template BS5 Sidebar code

--- PandaiNotes/src/App.js
+++ PandaiNotes/src/App.js
@@ -1,11 +1,24 @@
-import "./App.css";
-import Navbar from "./components/Navbar/Navbar";
+// import Navbar from "./components/UI/Navbar/Navbar";
+import classes from "./App.module.css";
+import DocumentEditor from "./components/DocumentEditor/DocumentEditor";
+
+import NavbarInternal from "./components/UI/Navbar/Navbar";
+
+import { React } from "react";
+import { Container, Row, Col } from "react-bootstrap";
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-    </div>
+    <Container fluid>
+      <Row className="vh-100">
+        <Col className="col-2">
+          <NavbarInternal />
+        </Col>
+        <Col>
+          <DocumentEditor />
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
