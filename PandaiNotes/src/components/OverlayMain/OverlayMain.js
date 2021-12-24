b1f433c7dd7b59d63c33dca6abcd8a2ca73108c5
Nigel Teo <nigelteosw@gmail.com>
2021-12-24 18:22:03 +0800

    linked pages to show on OverlayMain.js

--- PandaiNotes/src/components/OverlayMain/OverlayMain.js
+++ PandaiNotes/src/components/OverlayMain/OverlayMain.js
@@ -1,9 +1,44 @@
-import React, { useReducer } from "react";
+import React, { useState, useReducer } from "react";
+import { Card } from "react-bootstrap";
 
-const pageHandler = () => {};
+import moduleCard from "../SmallComponents/moduleCard/moduleCard";
+import todoPage from "../Pages/todoPage/todoPage";
+import fileExplorerPage from "../Pages/fileExplorerPage/fileExplorerPage";
+
+import { Container, Row, Col } from "react-bootstrap";
+
+const initialPage = "todo";
+
+// function reducer(state, action) {
+//   switch (action.type) {
+//     case 'todo':
+//       initialPage = "files"
+//       return ;
+//     case 'files':
+//       initialPage = "todo"
+//       return ;
+//     default:
+//       throw new Error();
+//   }
+// }
+
 
 const OverlayMain = (props) => {
-  const [currState, changePage] = useReducer(pageHandler);
+  const [overlayPage, setOverlayPage] = useState(true);
+  return (
+    <>
+      <Container>
+        <Row>
+          <Col>
+            {overlayPage
+              ? <todoPage />
+              : <fileExplorerPage />
+            }
+          </Col>
+        </Row>
+      </Container>
+    </>
+  )
 };
 
 export default OverlayMain;
---
+++ PandaiNotes/src/components/Pages/todoPage/todoPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+
+const todoPage = (props) => {
+    return (
+        <Container>
+            <div>To - Do</div>
+        </Container>
+    )
+}
+
+export default todoPage;
