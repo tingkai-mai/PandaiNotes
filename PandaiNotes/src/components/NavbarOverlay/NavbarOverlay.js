ccd4d54c5fb59e6491241e98b57025cf4de3bedd
Nigel Teo <nigelteosw@gmail.com>
2021-12-27 14:11:54 +0800

    better sidebar (improved css)

--- PandaiNotes/src/components/NavbarOverlay/NavbarOverlay.js
+++ PandaiNotes/src/components/NavbarOverlay/NavbarOverlay.js
@@ -9,7 +9,7 @@ function NavbarOverlay() {
 
     return (
         <Navbar className={`${classes["side-navbar"]} flex-column`}>
-          <Navbar.Brand href="#home">Pandai-Notes</Navbar.Brand>
+          <Navbar.Brand href="#home">Pandai-Notes</Navbar.Brand >
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto flex-column">
@@ -30,17 +30,6 @@ function NavbarOverlay() {
                   );
                 })}
               </Nav.Link>
-              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">
-                  Another action
-                </NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.4">
-                  Separated link
-                </NavDropdown.Item>
-              </NavDropdown>
             </Nav>
           </Navbar.Collapse>
         </Navbar>
