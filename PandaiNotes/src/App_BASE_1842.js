f3679e8d369fbca6cfd3d208bdb930f5641a7e09
Ferusel <wheatgrass.mc@gmail.com>
2021-12-28 08:17:43 +0800

    Merged conflicts
    Merged Nigel's and TK's work

---
+++ PandaiNotes/src/App_BASE_1842.js
@@ -0,0 +1,35 @@
+// import Navbar from "./components/UI/Navbar/Navbar";
+import classes from "./App.module.css";
+import DocumentEditor from "./components/DocumentEditor/DocumentEditor";
+import NavbarInternal from "./components/UI/Navbar/Navbar";
+import OverlayMain from "./components/OverlayMain/OverlayMain";
+
+import { React, useState } from "react";
+import { Container, Row, Col } from "react-bootstrap";
+
+
+
+function App() {
+
+
+  // Create overlay handler
+
+  const [overlayActive, setOverlayActive] = useState(true);
+  return (
+    <Container fluid>
+      <Row className="vh-100">
+        <Col className="col-2">
+          <NavbarInternal />
+        </Col>
+        <Col>
+          {overlayActive
+            ? <OverlayMain />
+            : <DocumentEditor />
+          }
+        </Col>
+      </Row>
+    </Container>
+  );
+}
+
+export default App;
