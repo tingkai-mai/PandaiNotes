8bf5cee9ff5910172fa95a6a3eb48ba4b55ac0e8
Ferusel <wheatgrass.mc@gmail.com>
2021-12-31 17:32:49 +0800

    DocumentEditor can now tag Important, Revision, Difficult tags
    DocumentEditor can now tag Important, Revision, Difficult items and retrieve these tags when needed using the grabTaggedHandler(). BUG: Can tag the same content with the same tag multiple times!!!

--- PandaiNotes/src/components/DocumentEditor/DocumentEditor.js
+++ PandaiNotes/src/components/DocumentEditor/DocumentEditor.js
@@ -1,7 +1,8 @@
-import { Editor, PluginManager } from "@tinymce/tinymce-react";
+import { Editor } from "@tinymce/tinymce-react";
 import tinymce from "tinymce/tinymce";
 import { useRef, useState, useEffect } from "react";
 import { SAMPLE_NOTE_BIG, SAMPLE_NOTE_SMALL } from "../../db/SAMPLE_NOTES_DB";
+import { Button } from "react-bootstrap";
 import { v4 as uuidv4 } from "uuid";
 
 // Change Me
@@ -9,16 +10,15 @@ const CHOSEN_NOTE = SAMPLE_NOTE_SMALL;
 
 const axios = require("axios");
 tinymce.PluginManager.add("tag-difficult", function (editor) {
-  console.log("setting up context-menu");
+  console.log("setting up context-menu tag-difficult");
   editor.ui.registry.addMenuItem("tag-difficult", {
-    text: "tag as difficult",
-    icon: "image",
+    text: "Tag as difficult",
+    icon: "bookmark",
     onAction: function () {
-      console.log("action called");
-      var comment = prompt("Annotate difficult content");
+      // var comment = prompt("Annotate difficult content");
       editor.annotator.annotate("difficult", {
         uid: uuidv4(),
-        comment: comment,
+        // comment: comment,
       });
       editor.focus();
     },
@@ -26,20 +26,68 @@ tinymce.PluginManager.add("tag-difficult", function (editor) {
 
   editor.ui.registry.addContextMenu("tag-difficult", {
     update: function (element) {
+      console.log("tagging as difficult");
       return "tag-difficult";
+    },
+  });
+});
+
+tinymce.PluginManager.add("tag-important", function (editor) {
+  console.log("setting up context-menu tag-important");
+  editor.ui.registry.addMenuItem("tag-important", {
+    text: "Tag as important",
+    icon: "bookmark",
+    onAction: function () {
+      // var comment = prompt("Annotate difficult content");
+      editor.annotator.annotate("important", {
+        uid: uuidv4(),
+        // comment: comment,
+      });
+      editor.focus();
+    },
+  });
+
+  editor.ui.registry.addContextMenu("tag-important", {
+    update: function (element) {
+      console.log("tagging as important");
+      return "tag-important";
+    },
+  });
+});
+
+tinymce.PluginManager.add("tag-revision", function (editor) {
+  console.log("setting up context-menu tag-revision");
+  editor.ui.registry.addMenuItem("tag-revision", {
+    text: "Tag as things to revise",
+    icon: "bookmark",
+    onAction: function () {
+      // var comment = prompt("Annotate difficult content");
+      editor.annotator.annotate("revision", {
+        uid: uuidv4(),
+        // comment: comment,
+      });
+      editor.focus();
+    },
+  });
+
+  editor.ui.registry.addContextMenu("tag-revision", {
+    update: function (element) {
+      console.log("tagging as revision");
+      return "tag-revision";
     },
   });
 });
 
 const settings = {
   selector: "textarea",
-  contextmenu: "tag-difficult link table forecolor backcolor",
+  contextmenu:
+    "link table | tag-difficult tag-important tag-revision |forecolor backcolor",
   toolbar: [
-    "difficult undo redo save | fontselect fontsizeselect | alignleft aligncenter alignright alignnone | formatselect | bullist numlist outdent indent | help ",
+    "grab-tags difficult important revision undo redo save | fontselect fontsizeselect | alignleft aligncenter alignright alignnone | formatselect | bullist numlist outdent indent | help ",
     "bold italic underline strikethrough forecolor backcolor cut paste | image media | visualblocks table anchor",
   ],
   plugins: [
-    "tag-difficult advlist autolink lists link image",
+    "tag-difficult tag-important tag-revision advlist autolink lists link image",
     "charmap print preview anchor help",
     "searchreplace visualblocks code",
     "insertdatetime media table paste wordcount",
@@ -48,41 +96,73 @@ const settings = {
   height: "100vh",
   menubar: false,
   resize: false,
-  content_style:
-    ".mce-annotation { background-color: darkgreen; color: white; } " +
-    "body { font-family:Helvetica,Arial,sans-serif; font-size:14px }",
+  content_style: `.mce-annotation { background-color: darkgreen; color: white; }
+    span[data-mce-annotation="difficult"] { background-color: yellow; color: black; } 
+    span[data-mce-annotation="important"] { background-color: red; color: white; } 
+    span[data-mce-annotation="revision"] { background-color: green; color: black; } 
+    body { font-family:Helvetica,Arial,sans-serif; font-size:14px }`,
 
   setup: function (editor) {
     console.log("setting up annotation");
-    editor.ui.registry.addButton("difficult", {
-      text: "Difficult topic",
-      onAction: function () {
-        var comment = prompt("Annotate difficult content");
-        editor.annotator.annotate("difficult", {
-          uid: uuidv4(),
-          comment: comment,
-        });
-        editor.focus();
-      },
-      onSetup: function (btnApi) {
-        editor.annotator.annotationChanged(
-          "difficult",
-          function (state, name, obj) {
-            console.log("Selecting difficult annotation: ", state);
-            btnApi.setDisabled(state);
-          }
-        );
-      },
-    });
+
+    // Convenience button to grab tags. Uncomment to see it at the top of the menu bar!
+    // Uncomment annotator.register("grab-tags", ...) as well
+    // editor.ui.registry.addButton("grab-tags", {
+    //   text: "grab-tags",
+    //   onAction: function () {
+    //     console.log(editor.annotator.getAll("difficult"));
+    //   },
+    //   onSetup: function (btnApi) {
+    //     editor.annotator.annotationChanged(
+    //       "difficult",
+    //       function (state, name, obj) {
+    //         console.log("Selecting difficult annotation: ", state);
+    //       }
+    //     );
+    //   },
+    // });
 
     editor.on("init", function () {
+      // editor.annotator.register("grab-tags", {
+      //   persistent: true,
+      //   decorate: function (uid, data) {
+      //     return {
+      //       attributes: {
+      //         "data-tagging-comment": data.comment ? data.comment : "",
+      //         // "data-tagging-author": data.author ? data.author : "anonymous",
+      //       },
+      //     };
+      //   },
+      // });
       editor.annotator.register("difficult", {
         persistent: true,
         decorate: function (uid, data) {
           return {
             attributes: {
-              "data-mce-comment": data.comment ? data.comment : "",
-              "data-mce-author": data.author ? data.author : "anonymous",
+              "data-tagging-comment": data.comment ? data.comment : "",
+              // "data-tagging-author": data.author ? data.author : "anonymous",
+            },
+          };
+        },
+      });
+      editor.annotator.register("important", {
+        persistent: true,
+        decorate: function (uid, data) {
+          return {
+            attributes: {
+              "data-tagging-comment": data.comment ? data.comment : "",
+              // "data-tagging-author": data.author ? data.author : "anonymous",
+            },
+          };
+        },
+      });
+      editor.annotator.register("revision", {
+        persistent: true,
+        decorate: function (uid, data) {
+          return {
+            attributes: {
+              "data-tagging-comment": data.comment ? data.comment : "",
+              // "data-tagging-author": data.author ? data.author : "anonymous",
             },
           };
         },
@@ -115,6 +195,15 @@ function DocumentEditor({ initialValue }) {
       console.log(content);
     }
   };
+
+  const grabTaggedHandler = () => {
+    const difficult = tinymce.get()[0].annotator.getAll("difficult");
+    const important = tinymce.get()[0].annotator.getAll("important");
+    const revision = tinymce.get()[0].annotator.getAll("revision");
+    console.log(difficult);
+    console.log(important);
+    console.log(revision);
+  };
   return (
     <>
       <Editor
@@ -128,6 +217,7 @@ function DocumentEditor({ initialValue }) {
         Save
       </button>
       {dirty && <p>You have unsaved content!</p>}
+      <Button onClick={grabTaggedHandler}>Grab tagged</Button>
     </>
   );
 }
